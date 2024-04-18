727a6f4e1cdc2df4a131d6e63a4c6f42e5722416
姚黎明 <yaoliming@kjteck.com>
2024-04-18 15:25:14 +0800

    feat: 功能提交

---
+++ app/ui/dashboard/sideNav/SideNav.tsx
@@ -0,0 +1,19 @@
+const SideNav = ()=>{
+    return (
+        <>
+        <div className="dataBox">
+            <div>SideNav 页面</div>
+            <div>111</div>
+            <div>333</div>
+            <div>666</div>
+        </div>
+
+        {/* <div className="flex container ">
+            <div className="flex-auto basis-20 grow shrink">222</div>
+
+        </div> */}
+        </>
+    )
+}
+
+export default SideNav
